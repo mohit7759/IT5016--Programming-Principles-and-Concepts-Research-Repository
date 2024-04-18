b8cf161fc8c8f58b3148651fae1deeb6eec8e118
mehakpreetsingh351 <165859739+mehakpreetsingh351@users.noreply.github.com>
2024-04-18 17:59:54 +0530

    Update arithmetic.py

--- Python notes/arithmetic.py
+++ Python notes/arithmetic.py
@@ -1,17 +1,14 @@
-"""
 apples = 1
 orange = 6
 fruit_basket = apples + oranges
 fruit_juice = apples // oranges
 
-print(
-    f"I have {fruit_basket} fruits in my basket. That is enough for {fruit_juice} rounds of fruit juice"
-)
-"""
+print(f"I have {fruit_basket} fruits in my basket. That is enough for {fruit_juice} rounds of fruit juice")
 
-# comparison Operators
 cookies_in_jar = 30
 chocolates_in_box = 15
 
 if cookies_in_jar > chocolates_in_box:
-    print("The cookie jar is winning! It")
+  print("The cookie jar is winning! It has", cookies_in_jar, "cookies, which is more than the", chocolates_in_box, "chocolates in the box.")
+else:
+  print("The chocolates are winning! There are", chocolates_in_box, "chocolates, which is more than the", cookies_in_jar, "cookies in the jar.")
