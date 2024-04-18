7426e3c30b8304f4d9e36fe77925e1a3f1c0f3d3
mehakpreetsingh351 <165859739+mehakpreetsingh351@users.noreply.github.com>
2024-04-18 17:57:09 +0530

    Add files via upload

---
+++ Python notes/arithmetic.py
@@ -0,0 +1,17 @@
+"""
+apples = 1
+orange = 6
+fruit_basket = apples + oranges
+fruit_juice = apples // oranges
+
+print(
+    f"I have {fruit_basket} fruits in my basket. That is enough for {fruit_juice} rounds of fruit juice"
+)
+"""
+
+# comparison Operators
+cookies_in_jar = 30
+chocolates_in_box = 15
+
+if cookies_in_jar > chocolates_in_box:
+    print("The cookie jar is winning! It")
